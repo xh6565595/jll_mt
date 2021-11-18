916a0d893176ba78b8805906a7829d2e7c5537f5
loserjohn <1292404844@qq.com>
2021-11-18 11:28:33 +0800

    pq测试

--- SET.js
+++ SET.js
@@ -7,7 +7,7 @@ export default {
 	// wx_appid: 'wx09daee2f47e178aa',    //微信授权 appid  測試公衆號
 	wx_appid: 'wx35c21791caf26593',    //代码力量 测试
 	// wx_redirect_url:'http://192.168.1.9:8080',
-	wx_redirect_url: 'https://gllo.kuxiong999.com',   //微信授权 回调页地址
-	
+	wx_redirect_url: 'https://gllo.kuxiong999.com',   //微信授权 回调页地址12
+	  
 	versionUrl:'/api/AppVersion/VersionCheck'   //app版本检测url   // 1是不更新 2是强制更新 3可选择更新 4//appstore更新
 } 
